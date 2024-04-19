1867891d7398f42994a751ed309740e9a175a8d8
bmuhamood <bmuhamood@edch.com>
2024-04-19 19:25:27 +0400

    added Navigation and completed Icons

--- Apps/Navigations/TabNavigation.jsx
+++ Apps/Navigations/TabNavigation.jsx
@@ -4,15 +4,38 @@ import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import BookingScreen from '../Screens/BookingScreen/BookingScreen';
 import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
 import { View, Text } from 'react-native';
+import Colors from '../Utils/Colors';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function CustomTabNavigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Booking" component={BookingScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator screenOptions={{headerShown:false}}>
+      <Tab.Screen name="home" component={HomeScreen} 
+      options={{
+        tabBarLabel:({color}) => (
+            <Text style={{color:color,fontSize:12,marginTop:-7}}>Home</Text>
+        ),
+        tabBarIcon:({color,size}) => <FontAwesome name="home" size={24} color="black" />
+      }}
+      />
+      <Tab.Screen name="booking" component={BookingScreen} 
+            options={{
+                tabBarLabel:({color}) => (
+                    <Text style={{color:color,fontSize:12,marginTop:-7}}>Booking</Text>
+                ),
+                tabBarIcon:({color,size}) => <FontAwesome name="bookmark" size={24} color="black" />
+              }}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} 
+            options={{
+                tabBarLabel:({color}) => (
+                    <Text style={{color:color,fontSize:12,marginTop:-7}}>Profile</Text>
+                ),
+                tabBarIcon:({color,size}) => <FontAwesome name="user-circle" size={24} color="black" />
+              }}
+      />
     </Tab.Navigator>
   );
 }
